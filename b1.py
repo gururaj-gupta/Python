5341c84ec2ebe708543c7a5032dd0d69326f353b
Gururaj Gupta <gururaj.gpt544@gmail.com>
2020-05-27 10:26:44 +0530

    cmt 27 may

--- b1.py
+++ b1.py
@@ -1,5 +1,29 @@
 #Coments begin with "#"
 a=5
 a=a+1
-b="Drink and Grow Rich "*a
+c=a+1
+b="Drink and Grow Rich "*a  #String printed 'a' number of times
 print(b)
+
+type(a)      #Returns data type
+
+print(a//c)  #The // operator gives us a result that's rounded down to the next integer.
+print(a**c)  #Raise to power (a^c in this case 6^7)
+
+#PEMDAS - Parentheses, Exponents, Multiplication/Division, Addition/Subtraction. Order of execution
+print(-4+4*2)
+
+#Parentheses are your useful here. 
+x=(10+3)/2
+print(x)
+#You can add them to force Python to evaluate sub-expressions in whatever order you want.
+
+#Build in Functions min, max, absolute(modulas (|-2|=2) )
+print(min(1,2,3))   #same for max
+print(32)
+print(abs(-32))
+
+#int and float can also be called as functions which convert their arguments to the corresponding type
+print(int(3.2))
+print(float(10))
+
